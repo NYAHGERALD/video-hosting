7cc398f28672a35eb55e561642c83d24d885af8d
Gerald Nyah <59947415+NYAHGERALD@users.noreply.github.com>
2025-07-08 23:14:34 -0500

    Fix: Use SQLALCHEMY_DATABASE_URI for production

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ CORS(app)
 
 
 # Config
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
